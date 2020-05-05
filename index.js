08d70c984bde4bda863493b20cf35cf8a4095490
amine.tchitooss <amine.tchitooss@gmail.com>
2020-05-05 20:25:34 +0200

    this is example

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 console.log('hello world')
-const z=2
+function mood() {
+    return ':)'
+}
+
+function text(param) {
+    const v = param
+    console.log('hello world', v)
+}
+
+function foo() {
+    console.log('howdy')
+    return text(mood())
+}
+
+
+foo()
+
